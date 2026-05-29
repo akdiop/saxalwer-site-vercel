3d728f690b5ab7e4b6ba0f13e8e1c00740c3f2c9
akdiop <aidalaye.diop@hotmail.com>
2026-05-29 12:04:27 +0000

    Update NewsletterForm.tsx

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -1,5 +1,5 @@
 import { useState, FormEvent } from 'react';
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion'
 import { projectId, publicAnonKey } from '../utils/supabase/info';
 import { Language, getTranslation } from '../i18n/translations';
 
